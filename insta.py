82af45a1b61c3e51bfd0694ce43a5b636b871abe
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-05-16 15:51:06 +0400

    Update insta.py

--- insta.py
+++ insta.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
-    print(f"@{uname} Started Successfully!")
+    print(f"@{uname} Başarıyla Başladı!")
     idle()
     app.stop()
-    print("Bot stopped. Alvida!")
+    print("Bot durdu.!")
