748dccb2da8d3f10f42085084bf466501d94455f
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-05-16 15:15:18 +0400

    Update auth.py

--- Instaloader/auth.py
+++ Instaloader/auth.py
@@ -5,21 +5,20 @@ from .database.users_sql import set_info, delete_info
 
 @Client.on_message(filters.private & filters.incoming & filters.command("auth"))
 async def _auth(bot, msg):
-    await msg.reply("**Authorize only if you trust us** \n\n"
-                    "1) For authorizing you need to provide instagram username and password for your account "
-                    "when asked.\n"
-                    "2) We would suggest you to make a new account for this purpose as instagram can ban your "
-                    "account for downloading content. \n\n"
-                    "**Note** : We are not responsible for account bans")
+    await msg.reply("**Yalnızca bize güveniyorsanız yetkilendirin** \n\n"
+                    "1) Yetki vermek için istendiğinde hesabınız için instagram kullanıcı adı ve şifre sağlamanız gerekir."\n\n"
+                    "2) Bu amaçla yeni bir hesap açmanızı öneririz çünkü instagram hesabınızı yasaklayabilir. "
+                    ". \n\n"
+                    "**Not** : Hesap yasaklarından sorumlu değiliz")
     confirmation = await bot.ask(msg.from_user.id,
-                                 "**Do you wish to proceed?** \n\nSend '`yes`' or '`y`' for positive "
-                                 "confirmation.\nSend '`no`' or '`n`' to cancel authorization.")
+                                 "**Devam etmek istiyor musunuz?** \n\n '`yes`' veya '`y`' yazın "
+                                 "Onayla.\n iptal etmek için '`no`' veya '`n`' Yazın.")
     if not confirmation.text.lower() in ['yes', 'y']:
-        await confirmation.reply("Authorization Cancelled", quote=True)
+        await confirmation.reply("Yetkilendirme İptal Edildi", quote=True)
         await msg.stop_propagation()
         return
-    username = await bot.ask(msg.from_user.id, "Please send your Instagram Username", filters=filters.user(msg.from_user.id))
-    password = await bot.ask(msg.from_user.id, "Please send your Instagram Password", filters=filters.user(msg.from_user.id))
+    username = await bot.ask(msg.from_user.id, "Lütfen Instagram kullanıcı adınızı gönderin", filters=filters.user(msg.from_user.id))
+    password = await bot.ask(msg.from_user.id, "Lütfen Instagram Şifrenizi gönderin", filters=filters.user(msg.from_user.id))
     await msg.reply("Checking if login credentials are valid...")
     command = f"instaloader -l {username.text} -p {password.text}"
     proc = await asyncio.subprocess.create_subprocess_shell(
@@ -30,17 +29,17 @@ async def _auth(bot, msg):
     stdout, stderr = await proc.communicate()
     if "login error" in str(stderr).lower():
         await msg.reply(f'Log in failed. \n\n{str(stderr.decode("utf-8")).replace("Fatal error: Login error: ", "")} '
-                        f'\nPlease try authorizing again with /auth.')
+                        f'\n Lütfen şununla yeniden yetkilendirmeyi deneyin: /auth.')
         return
     await set_info(msg.from_user.id, username.text, password.text)
-    await msg.reply("Authorization was successful. You can download private posts now!")
+    await msg.reply("Yetkilendirme başarılı oldu. Özel gönderileri şimdi indirebilirsiniz!")
 
 
 @Client.on_message(filters.private & filters.incoming & filters.command("unauth"))
 async def _unauth(_, msg):
     success = await delete_info(msg.from_user.id)
     if success:
-        await msg.reply("Your credentials have been deleted from my database. \n\n"
-                        "Now I can't access your account and you can't download private posts")
+        await msg.reply("Kimlik bilgileriniz veritabanımdan silindi. \n\n"
+                        "Artık hesabınıza erişemiyorum ve özel gönderileri indiremiyorsunuz")
     else:
-        await msg.reply("You didn't authorize me anyway!")
+        await msg.reply("Yine de bana yetki vermedin!")
