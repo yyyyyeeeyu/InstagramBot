3b490ad852ddbb55a8c174799da3f6d05077fb75
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-05-16 15:33:25 +0400

    Update profile_pic.py

--- Instaloader/profile_pic.py
+++ Instaloader/profile_pic.py
@@ -7,13 +7,13 @@ from instaloader.exceptions import QueryReturnedNotFoundException, ProfileNotExi
 
 @Client.on_message(filters.private & filters.command(["profile_pic", "dp"]))
 async def dp(_, msg):
-    status = await msg.reply('Please Wait...', quote=True)
+    status = await msg.reply('Lütfen bekleyin...', quote=True)
     if len(msg.command) == 1:
-        await msg.reply("Please do not use empty command. Below is the right format to get a profile pic. "
-                        "\n\n`/profile_pic instagram-username` \n\nExample : `/profile_pic taaarannn`")
+        await msg.reply("Lütfen boş komut kullanmayın. Profil resmi almak için doğru format aşağıdadır. "
+                        "\n\n`/profile_pic instagram-username` \n\nMisal : `/profile_pic thebloodper`")
         return
     elif len(msg.command) > 2:
-        await msg.reply("Use 1 username at a time.")
+        await msg.reply("Bir seferde 1 kullanıcı adı kullanın.")
         return
     text = msg.command[1]
     if text.startswith('@'):
@@ -22,12 +22,12 @@ async def dp(_, msg):
         instaloader.Instaloader().download_profile(text, profile_pic_only=True)
     except (QueryReturnedNotFoundException, ProfileNotExistsException):
         await status.delete()
-        await msg.reply("No Such Instagram Account Exists.", quote=True)
+        await msg.reply("Böyle Bir Instagram Hesabı Yok.", quote=True)
         return
     files = os.listdir(text.lower())
     for file in files:
         if file.endswith(".jpg"):
-            caption = f"Profile Picture of [@{text}](https://instagram.com/{text}) \n\nBy @StarkBots"
+            caption = f"Profil Resmi [@{text}](https://instagram.com/{text}) \n\n @EpicEyeBots"
             await msg.reply_photo(f"{text}/{file}", caption=caption)
             await status.delete()
     shutil.rmtree(text)
